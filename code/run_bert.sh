732139feadf34033879c740e87467a66519a2bc5
helen <519577403@qq.com>
2019-05-13 13:41:57 -0400

    run bert script timeout

--- code/run_bert.sh
+++ code/run_bert.sh
@@ -2,7 +2,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
-#SBATCH -t10:00:00
+#SBATCH -t50:00:00
 #SBATCH --mem=20GB
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jl10005@nyu.edu
@@ -15,4 +15,4 @@ module load python3/intel/3.6.3
 source ~/pyenv/py3.6.3/bin/activate
 
 #python train_man_exp3.py --dataset amazon-lang --model lstm --max_epoch 20
-python train_man_bert.py --dataset amazon-lang --model dan --max_epoch 20 --model_save_file ./save/train_bert --topic_domain all --domains de en fr --no_wgan_trick --sum_pooling --shared_hidden_size 768
+python train_man_bert.py --dataset amazon-lang --model dan --max_epoch 14 --model_save_file ./save/train_bert --topic_domain all --domains de en fr --no_wgan_trick --sum_pooling --shared_hidden_size 768
