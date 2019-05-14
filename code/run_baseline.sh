e5e0d8885a80be854065a0c32ab161beb578f48b
helen <519577403@qq.com>
2019-05-14 15:39:30 -0400

    seperate training

--- code/run_baseline.sh
+++ code/run_baseline.sh
@@ -15,7 +15,7 @@ module load python3/intel/3.6.3
 source ~/pyenv/py3.6.3/bin/activate
 
 #python train_man_exp3.py --dataset amazon-lang --model lstm --max_epoch 20
-python train_man_baselines.py --dataset amazon-lang --model dan --max_epoch 2 --model_save_file ./save/train_baselines_shared --topic_domain all --domains de en fr --no_wgan_trick --sum_pooling --shared
-python train_man_baselines.py --dataset amazon-lang --model dan --max_epoch 2 --model_save_file ./save/train_baselines_shared_man --topic_domain all --domains de en fr --no_wgan_trick --sum_pooling --shared_man
-python train_man_baselines.py --dataset amazon-lang --model dan --max_epoch 2 --model_save_file ./save/train_baselines_private --topic_domain all --domains de en fr --no_wgan_trick --sum_pooling --private
-python train_man_baselines.py --dataset amazon-lang --model dan --max_epoch 2 --model_save_file ./save/train_baselines_shared_private_man --topic_domain all --domains de en fr --no_wgan_trick --sum_pooling --shared_private_man
+python train_man_baselines.py --dataset amazon-lang --model dan --max_epoch 20 --model_save_file ./save/train_baselines_shared --topic_domain all --domains de en fr --no_wgan_trick --sum_pooling --shared
+# python train_man_baselines.py --dataset amazon-lang --model dan --max_epoch 20 --model_save_file ./save/train_baselines_shared_man --topic_domain all --domains de en fr --no_wgan_trick --sum_pooling --shared_man
+# python train_man_baselines.py --dataset amazon-lang --model dan --max_epoch 20 --model_save_file ./save/train_baselines_private --topic_domain all --domains de en fr --no_wgan_trick --sum_pooling --private
+# python train_man_baselines.py --dataset amazon-lang --model dan --max_epoch 20 --model_save_file ./save/train_baselines_shared_private_man --topic_domain all --domains de en fr --no_wgan_trick --sum_pooling --shared_private_man
--- code/run_bert.sh
+++ code/run_bert.sh
@@ -6,7 +6,7 @@
 #SBATCH --mem=20GB
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jl10005@nyu.edu
-#SBATCH --job-name=test_bert
+#SBATCH --job-name=run-bert
 #SBATCH --gres=gpu:p40:1
 #SBATCH --output=slurm_%j.out
 
@@ -15,4 +15,7 @@ module load python3/intel/3.6.3
 source ~/pyenv/py3.6.3/bin/activate
 
 #python train_man_exp3.py --dataset amazon-lang --model lstm --max_epoch 20
-python train_man_bert.py --dataset amazon-lang --model dan --max_epoch 14 --model_save_file ./save/train_bert --topic_domain all --domains de en fr --no_wgan_trick --sum_pooling --shared_hidden_size 768
+python train_man_bert.py --dataset amazon-lang --model dan --max_epoch 14 --model_save_file ./save/train_bert_shared --topic_domain all --domains de en fr --no_wgan_trick --sum_pooling --shared_hidden_size 768 --shared
+# python train_man_bert.py --dataset amazon-lang --model dan --max_epoch 14 --model_save_file ./save/train_bert_shared_man --topic_domain all --domains de en fr --no_wgan_trick --sum_pooling --shared_hidden_size 768 --shared_man
+# python train_man_bert.py --dataset amazon-lang --model dan --max_epoch 14 --model_save_file ./save/train_bert_private --topic_domain all --domains de en fr --no_wgan_trick --sum_pooling --shared_hidden_size 768 --private
+# python train_man_bert.py --dataset amazon-lang --model dan --max_epoch 14 --model_save_file ./save/train_bert_shared_private_man --topic_domain all --domains de en fr --no_wgan_trick --sum_pooling --shared_hidden_size 768 --shared_private_man
